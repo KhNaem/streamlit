c95a5d624c92195373bee058558ff0a226f19e34
KhNaem <91856408+KhNaem@users.noreply.github.com>
2025-05-14 09:00:03 +0700

    Update แก้ hour ที่เหลือ.py

--- แก้ hour ที่เหลือ.py
+++ แก้ hour ที่เหลือ.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Brush Dashboard", layout="wide")
 service_account_info = st.secrets["gcp_service_account"]
 creds = Credentials.from_service_account_info(service_account_info, scopes=["https://www.googleapis.com/auth/spreadsheets"])
 gc = gspread.authorize(creds)
-sheet_url = "https://docs.google.com/spreadsheets/d/1Pd6ISon7-7n7w22gPs4S3I9N7k-6uODdyiTvsfXaSqY/edit?usp=sharing"
+sheet_url = "https://docs.google.com/spreadsheets/d/1SOkIH9jchaJi_0eck5UeyUR8sTn2arndQofmXv5pTdQ"
 sh = gc.open_by_url(sheet_url)
 
 sheet_names = [ws.title for ws in sh.worksheets()]
